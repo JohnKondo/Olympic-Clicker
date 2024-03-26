e3fd24d81b9f83f444fb6029e305f4677e08ef54
John Kondozopulos <john.kondozopulos@epita.fr>
2024-03-26 16:16:48 +0100

    on touch start for ios devices

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import { FBXLoader } from 'three/addons/loaders/FBXLoader.js';
 
 
 let camera, scene, renderer, stats;
-let colors = [new THREE.Color( 0xa0ffff ), new THREE.Color( 0xffff0a )];
+let colors = [new THREE.Color(0xa0ffff), new THREE.Color(0xffff0a)];
 let i = 0;
 let started = false;
 const startSpeed = -0.001;
@@ -25,27 +25,27 @@ animate();
 
 function init() {
 
-	const container = document.createElement( 'div' );
-	document.body.appendChild( container );
+	const container = document.createElement('div');
+	document.body.appendChild(container);
 
-	camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
-	camera.position.set( 3, 325, -525 );
+	camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
+	camera.position.set(3, 325, -525);
 
 	scene = new THREE.Scene();
-	scene.background = new THREE.Color( 0xa0a0ff );
+	scene.background = new THREE.Color(0xa0a0ff);
 
-	const hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444, 5 );
-	hemiLight.position.set( 0, 200, 0 );
-	scene.add( hemiLight );
+	const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444, 5);
+	hemiLight.position.set(0, 200, 0);
+	scene.add(hemiLight);
 
-	const dirLight = new THREE.DirectionalLight( 0xffffff, 5 );
-	dirLight.position.set( 0, 200, 100 );
+	const dirLight = new THREE.DirectionalLight(0xffffff, 5);
+	dirLight.position.set(0, 200, 100);
 	dirLight.castShadow = true;
 	dirLight.shadow.camera.top = 180;
 	dirLight.shadow.camera.bottom = - 100;
 	dirLight.shadow.camera.left = - 120;
 	dirLight.shadow.camera.right = 120;
-	scene.add( dirLight );
+	scene.add(dirLight);
 
 	// Charger la texture de piste et créer un matériau
 	const trackMaterial = new THREE.MeshBasicMaterial({
@@ -61,40 +61,41 @@ function init() {
 
 	// model
 	const loader = new FBXLoader();
-	loader.load( 'public/run_in_place.fbx', function ( object ) {
-		mixer = new THREE.AnimationMixer( object );
-		action = mixer.clipAction( object.animations[ 0 ] );
+	loader.load('public/run_in_place.fbx', function (object) {
+		mixer = new THREE.AnimationMixer(object);
+		action = mixer.clipAction(object.animations[0]);
 		action.play();
-		object.traverse( function ( child ) {
-			if ( child.isMesh ) {
+		object.traverse(function (child) {
+			if (child.isMesh) {
 				child.castShadow = true;
 				child.receiveShadow = true;
 			}
-		} );
-		scene.add( object );
-	} );
+		});
+		scene.add(object);
+	});
 
-	renderer = new THREE.WebGLRenderer( { antialias: true } );
-	renderer.setPixelRatio( window.devicePixelRatio );
-	renderer.setSize( window.innerWidth, window.innerHeight );
+	renderer = new THREE.WebGLRenderer({ antialias: true });
+	renderer.setPixelRatio(window.devicePixelRatio);
+	renderer.setSize(window.innerWidth, window.innerHeight);
 	renderer.shadowMap.enabled = true;
-	container.appendChild( renderer.domElement );
+	container.appendChild(renderer.domElement);
 
-	const controls = new OrbitControls( camera, renderer.domElement );
-	controls.target.set( 0, 100, 0 );
-	controls.enabled  = false;
+	const controls = new OrbitControls(camera, renderer.domElement);
+	controls.target.set(0, 100, 0);
+	controls.enabled = false;
 	controls.update();
 
-	window.addEventListener( 'resize', onWindowResize );
+	window.addEventListener('resize', onWindowResize);
 
 	// stats
 	stats = new Stats();
-	container.appendChild( stats.dom );
-	const btn = document.createElement( 'button' );
+	container.appendChild(stats.dom);
+	const btn = document.createElement('button');
+	btn.ontouchstart = function () { btn.click() }
 	btn.innerHTML = 'Run';
 	btn.id = "run-button"
 	btn.classList.add("bulat-merah");
-	btn.addEventListener("click", function() {
+	btn.addEventListener("click", function () {
 		started = true;
 		scene.background = colors[i++ % 2];
 		if (currentSpeed < maxSpeed)
@@ -111,23 +112,24 @@ function init() {
 		}
 		setTimeout(decrement, 500)
 	}, 500);
-	container.appendChild( btn );
+	container.appendChild(btn);
 }
 
 function onWindowResize() {
 	camera.aspect = window.innerWidth / window.innerHeight;
 	camera.updateProjectionMatrix();
-	renderer.setSize( window.innerWidth, window.innerHeight );
+	renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
 function animate() {
-	requestAnimationFrame( animate );
+	requestAnimationFrame(animate);
 	const delta = clock.getDelta();
 	if (started == true) {
+		mixer.update(delta);
 		sphereGeometry.rotateX(currentSpeed);
 	}
-	if (mixer) 
-		mixer.update( delta );
-	renderer.render( scene, camera );
+	else
+		mixer.update(0);
+	renderer.render(scene, camera);
 	// stats.update();
 }
